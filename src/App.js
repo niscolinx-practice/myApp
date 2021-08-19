793d31363d722b553679fa20061da60e3ee8ccf3
Igboanugwo Collins <munisco12@gmail.com>
2021-08-19 11:11:28 +0100

    fixed the installation issue with yarn

--- src/App.js
+++ src/App.js
@@ -5,16 +5,19 @@ class App extends Component {
   render(){
     return (
         <div className='App'>
-            <h1>Hi, this is my react app</h1>
-            <p>
+        <h1>Hi, this is my react app</h1>
+        <ol>
+
+            <li>
                 Solved the installation issue by installing{' '}
                 <strong>yarn</strong> with{' '}
                 <strong>npm install --global yarn</strong>
-        </p>
+        </li>
+        <li>Also added a public folder which contains an <strong>index.html</strong></li>
+        </ol>
         <ul>
           <li><strong>Note:</strong> This can also be solved using npm rather than yarn but i had network issues, so had to consider yarn</li>
         </ul>
-        <p>Also added a public folder which contains an <strong>index.html</strong></p>
         <h3>Happy Coding!</h3>
         </div>
     )
